f43e81f910c4ae26d16485326ad1bd9a8d42fa10
Raphael Halmas Silva <raphaelhalmas@gmail.com>
2021-06-25 20:57:52 -0300

    Alterar a logo do painel de administracao

--- admin/src/components/LeftMenu/LeftMenuHeader/Wrapper.js
+++ admin/src/components/LeftMenu/LeftMenuHeader/Wrapper.js
@@ -5,27 +5,15 @@ import Logo from "../../../assets/images/logo-won.svg";
 
 const Wrapper = styled.div`
   background-color: #007eff;
-  padding-left: 2rem;
   height: ${props => props.theme.main.sizes.leftMenu.height};
-
-  .leftMenuHeaderLink {
-    &:hover {
-      text-decoration: none;
-    }
-  }
 
   .projectName {
     display: block;
-    width: 100%;
     height: ${props => props.theme.main.sizes.leftMenu.height};
-    font-size: 2rem;
-    letter-spacing: 0.2rem;
-    color: $white;
-
     background-image: url(${Logo});
     background-repeat: no-repeat;
-    background-position: left center;
-    background-size: auto 2.5rem;
+    background-position: center center;
+    background-size: 12rem;
   }
 `;
 
