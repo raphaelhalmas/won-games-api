b1f8c70c1c3ce4106962c82cb81145d84c9824ac
Raphael Halmas Silva <raphaelhalmas@gmail.com>
2021-07-06 00:05:49 -0300

    Ajustar scrapper para inclusão de dados das developers, publishers, categories e platforms

--- api/game/controllers/game.js
+++ api/game/controllers/game.js
@@ -8,9 +8,7 @@
 module.exports = {
     populate: async (ctx) => {
         console.log("Starting to populate...")
-
         await strapi.services.game.populate()
-
         ctx.send("Finished populating!")
     }
 };
--- api/game/services/game.js
+++ api/game/services/game.js
@@ -39,22 +39,53 @@ async function create(name, entityName) {
     }
 }
 
+async function createManyToManyData(products) {
+    const developers = {}
+    const publishers = {}
+    const categories = {}
+    const platforms = {}
+
+    products.forEach((product) => {
+        const { developer, publisher, supportedOperatingSystems, genres } = product
+    
+        genres && genres.forEach((item) => {
+            categories[item] = true
+        })
+    
+        supportedOperatingSystems && supportedOperatingSystems.forEach((item) => {
+            platforms[item] = true
+        })
+    
+        developers[developer] = true
+        publishers[publisher] = true
+    })
+
+    return Promise.all([
+        ...Object.keys(developers).map((name) => create(name, "developer")),
+        ...Object.keys(publishers).map((name) => create(name, "publisher")),
+        ...Object.keys(categories).map((name) => create(name, "category")),
+        ...Object.keys(platforms).map((name) => create(name, "platform")),
+    ]);
+}
+
 module.exports = {
     populate: async (params) => {
-        // const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`
-        // console.log(products[0])
-
-        const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&search=streets+of+rage&sort=popularity`
-        const { data: { products } } = await axios.get(gogApiUrl)        
-        // console.log(await getGameInfo(products[0].slug))
-
-        const { developer, publisher, slug } = products[0]
-        // await strapi.services.publisher.create({ name: publisher, slug: slugify(publisher).toLowerCase() })
-        // await strapi.services.developer.create({ name: developer, slug: slugify(developer).toLowerCase() })
-
-        // console.log(await getByName("DotEmu", "publisher"))
-
-        await create(publisher, "publisher")
-        await create(developer, "developer")
+        // const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&search=streets+of+rage&sort=popularity`
+        // const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&search=batman+knight+premium&sort=popularity`
+        // const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&search=gato+roboto&sort=popularity`
+        // const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&search=mother+russia+bleeds&sort=popularity`
+        // const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&search=ape+out&sort=popularity`
+        // const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&search=narita&sort=popularity`
+        // const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&search=scourge&sort=popularity`
+        // const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&search=kunai&sort=popularity`
+        // const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&search=children+of+morta&sort=popularity`
+        // const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&search=downwell&sort=popularity`
+        // const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&search=guacamelee&sort=popularity`
+        // const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&search=ori+and+the+blind&sort=popularity`
+        // const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&search=hollow&sort=popularity`
+        const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&search=guns+gore&sort=popularity`
+        const { data: { products } } = await axios.get(gogApiUrl)    
+        await createManyToManyData(products)
+        
     }
 };
